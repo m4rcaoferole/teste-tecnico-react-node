822c169816761f3035f79f470b909b8cb123655a
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-02-02 23:17:28 -0300

    feat: initial-sessions-checkpassword

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,3 +1,18 @@
-class SessionController {}
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+import { checkPassword } from '../services/auth';
+
+import authConfig from '../config/auth';
+class SessionController {
+  async create(req, res) {
+    const { email, password } = req.boby;
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User / Password invalid.' });
+    }
+  }
+}
 
 export default new SessionController();
